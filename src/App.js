d9568f6e25cc9043f21077cafc48750e288475e0
sonalminj <sonalminjprayas@gmail.com>
2023-11-16 19:19:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-// import Resturant from "./component/Basics/Resturant"; 
-// import UseState from "./component/Hooks/UseState.js";
-import UseEffect from "./component/Hooks/UseEffect.js";
-import UseReducer from "./component/Hooks/UseReducer.js"
+import Todo from "./component/todoreact/todo";
 
 
 const App = () => {
   return (
-    <UseReducer/>
+    <Todo/>
 
   )
 }
